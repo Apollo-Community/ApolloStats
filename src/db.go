6d292b0ff477bbcd6d67322871f21aebcbe32955
A. Svensson <lmasvensson@gmail.com>
2015-12-22 13:54:03 +0100

    Added basic db interface and models.

---
+++ src/db.go
@@ -0,0 +1,39 @@
+package apollostats
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+// NOTE: DON'T USE ANY WRITE OPERATIONS ON THE DATABASE!
+// We're interfacing with an external, live game database!
+
+type DB struct {
+	*gorm.DB
+}
+
+func OpenDB(args ...interface{}) (*DB, error) {
+	db, e := gorm.Open("mysql", args...)
+	if e != nil {
+		return nil, e
+	}
+	return &DB{&db}, nil
+}
+
+func (db *DB) AllBans() []*Ban {
+	var tmp []*Ban
+	db.Order("id desc").Limit(MAX_ROWS).Find(&tmp)
+	return tmp
+}
+
+func (db *DB) AllAccountItems() []*AccountItem {
+	var tmp []*AccountItem
+	db.Order("id desc").Limit(MAX_ROWS).Find(&tmp)
+	return tmp
+}
+
+func (db *DB) AllDeaths() []*Death {
+	var tmp []*Death
+	db.Order("id desc").Limit(MAX_ROWS).Find(&tmp)
+	return tmp
+}
---
+++ src/models.go
@@ -0,0 +1,58 @@
+package apollostats
+
+import (
+	"strings"
+	"time"
+)
+
+const MAX_ROWS = 100
+
+type Ban struct {
+	ID         int64     `gorm:"column:id;primary_key"`
+	Timestamp  time.Time `gorm:"column:bantime"`
+	CKey       string    `gorm:"column:ckey"`
+	CID        string    `gorm:"column:computerid"`
+	IP         string    `gorm:"column:ip"`
+	Bantype    string    `gorm:"column:bantype"`
+	Admin      string    `gorm:"column:a_ckey"`
+	Reason     string    `gorm:"column:reason"`
+	Duration   int       `gorm:"column:duration"`
+	Expiration time.Time `gorm:"column:expiration_time"`
+}
+
+func (b *Ban) TableName() string {
+	return "ban"
+}
+
+type AccountItem struct {
+	ID        int64     `gorm:"column:id;primary_key"`
+	Timestamp time.Time `gorm:"column:time"`
+	CKey      string    `gorm:"column:ckey"`
+	Item      string    `gorm:"column:item"`
+}
+
+func (a *AccountItem) TableName() string {
+	return "acc_items"
+}
+
+type Death struct {
+	ID        int64     `gorm:"column:id;primary_key"`
+	Timestamp time.Time `gorm:"column:tod"`
+	Name      string    `gorm:"column:name"`
+	Job       string    `gorm:"column:job"`
+	Room      string    `gorm:"column:pod"`
+	Position  string    `gorm:"column:coord"`
+	Brute     int       `gorm:"column:bruteloss"`
+	Brain     int       `gorm:"column:brainloss"`
+	Fire      int       `gorm:"column:fireloss"`
+	Oxygen    int       `gorm:"column:oxyloss"`
+}
+
+func (d *Death) TableName() string {
+	return "death"
+}
+
+func (d *Death) RoomName() string {
+	// Cleanup the room name
+	return strings.Trim(d.Room, "Ã¿")
+}
